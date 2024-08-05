68b72a90a5ecc6ca9e3b473db2d537040921254c
Mohammed Bilal N S <bilalnsmuhammed@gmail.com>
2024-08-05 19:13:58 +0530

    changed theme into tokyonight

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require('lualine').setup{
 			options ={
-				theme = 'ayu'
+				theme =  'ayu_mirage' -- 'codedark'
 			},
 			tabline = {
   lualine_a = {'buffers'},
