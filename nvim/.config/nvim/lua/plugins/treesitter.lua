f336d7702591438131ab97f34030185ac368532e
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2025-03-27 17:14:50 +0530

    chore: replaced wlsunset with gammastep, added TypeScript and JavaScript tree-sitters

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,20 @@
-return{
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-
-		require'nvim-treesitter.configs'.setup {
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "latex", "rust", "bash" },
-			highlight = { enable = true,
-				disable = { "latex"}
-			},
-			indent = {enable = true},
-
-
-		}
-
-
-
-	end
-
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      ensure_installed = {
+        "c", "lua", "vim", "vimdoc", "latex", "rust", "bash",
+        "javascript", "typescript", "tsx"
+      },
+      highlight = {
+        enable = true,
+        disable = { "latex" },
+      },
+      indent = {
+        enable = true,
+      },
+    }
+  end
 }
+
