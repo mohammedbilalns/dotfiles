3e633bb423a660974026b5edcaf2341cdb2e38b5
Mohammed Bilal <bilalnsmuhammed@gmail.com>
2026-01-03 16:27:54 +0530

    removed unused configs

--- niri/.config/niri/scripts/idle_inhibitor.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-if pgrep swayidle
-then
-	killall swayidle & notify-send "Idle inhibitor is active"
-else 
-	swayidle -w & notify-send "Idle inhibitor deactivated"
-fi
-
---
+++ nvim/.config/nvim/lua/plugins/non-ls.lua
@@ -0,0 +1,10 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      -- your config
+    })
+  end,
+}
